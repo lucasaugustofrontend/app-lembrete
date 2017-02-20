1c2bf1155a3614abcf9cfb868c2bad36035e1f0d
Lucas Augusto <lucas.augusto5061@gmail.com>
2017-02-20 20:03:42 -0300

    removido a biblioteca handlebars

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,24 +1,2 @@
-'use strict';
-
-function selector(elem) {
-  return document.querySelector(elem);
-}
-function compileHandleBars(elem, context, content) {
-  var source = selector(elem).innerHTML; // template handlebars
-  var template = Handlebars.compile(source);
-  var templateCompiled = template(context);
-  return content.innerHTML = templateCompiled;
-}
-
-var context = {
-  name: 'Larissa',
-  sname: 'Isabel'
-};
-var data = { person: [{ name: 'Lucas', sname: 'Augusto' }, { name: 'Larissa', sname: 'Isabel' }, { name: 'Maria', sname: 'Augusta' }, { name: 'Carlos', sname: 'Pereira' }]
-};
-var content = selector('.content');
-var person = selector('.person');
-compileHandleBars('#people', context, content);
-
-compileHandleBars('#person', data, person);
+"use strict";
 //# sourceMappingURL=main.js.map
